928f29dc7dc16b12c9663014a409d0ea1ef2618c
Udara Alwis <udara.alwis@swatchgroup.com>
2023-03-07 11:36:42 +0100

    finished the xylophone app

--- flutter_xylophone_app/lib/main.dart
+++ flutter_xylophone_app/lib/main.dart
@@ -8,22 +8,50 @@ void main() {
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({super.key});
 
+  void playSound(int keyValue) {
+    var player = AudioPlayer();
+    player.play(AssetSource('note$keyValue.wav'));
+  }
+
+  Expanded buildKeyButton({required int keyValue}) {
+    const keyValueColorMap = {
+      1: Colors.red,
+      2: Colors.orange,
+      3: Colors.yellow,
+      4: Colors.green,
+      5: Colors.teal,
+      6: Colors.blue,
+      7: Colors.purple,
+    };
+
+    return Expanded(
+      child: MaterialButton(
+        onPressed: () {
+          playSound(keyValue);
+        },
+        color: keyValueColorMap[keyValue] ?? Colors.black,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final player = AudioPlayer();
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-            child: Center(
-          child: TextButton(
-              onPressed: () {
-                // final audioCache = AudioCache();
-                // audioCache.load('note1.wav');
-                player.pause();
-                player.play(AssetSource('note1.wav'));
-              },
-              child: const Text('Click me')),
-        )),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKeyButton(keyValue: 1),
+              buildKeyButton(keyValue: 2),
+              buildKeyButton(keyValue: 3),
+              buildKeyButton(keyValue: 4),
+              buildKeyButton(keyValue: 5),
+              buildKeyButton(keyValue: 6),
+              buildKeyButton(keyValue: 7),
+            ],
+          ),
+        ),
       ),
     );
   }
