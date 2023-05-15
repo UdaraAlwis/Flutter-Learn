325a6146dff171b79b3b28d20355b80a67226e02
Udara Alwis <udara.robotics@gmail.com>
2023-05-15 08:30:55 +0200

    refact

--- flutter_beamer_nav_app/lib/main.dart
+++ flutter_beamer_nav_app/lib/main.dart
@@ -7,133 +7,45 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  final routerDelegate = BeamerDelegate(
-    locationBuilder: BeamerLocationBuilder(
-      beamLocations: [
-        HomeLocation(),
-        SettingsLocation(),
-      ],
+  MyApp({super.key});
+
+  var routerDelegate = BeamerDelegate(
+    locationBuilder: RoutesLocationBuilder(
+      routes: {'*': (context, state, data) => const ScaffoldWithBottomNavBar()},
     ),
   );
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'Beamer Example',
-      routerDelegate: routerDelegate,
-      routeInformationParser: BeamerParser(),
+      theme: ThemeData(primarySwatch: Colors.amber),
     );
   }
 }
 
-final selectedTabIndexProvider = StateProvider<int>((ref) => 0);
+class ScaffoldWithBottomNavBar extends StatefulWidget {
+  const ScaffoldWithBottomNavBar({super.key});
 
-class HomeLocation extends BeamLocation {
   @override
-  List<BeamPage> buildPages(BuildContext context, BeamState state) {
-    return [
-      BeamPage(
-        key: ValueKey('home'),
-        child: HomePage(),
-      ),
-      if (state.uri.pathSegments.contains('details'))
-        BeamPage(
-          key: ValueKey('details'),
-          child: DetailsPage(),
-        ),
-    ];
-  }
+  State<ScaffoldWithBottomNavBar> createState() =>
+      _ScaffoldWithBottomNavBarState();
 }
 
-class SettingsLocation extends BeamLocation {
-  @override
-  List<BeamPage> buildPages(BuildContext context, BeamState state) {
-    return [
-      BeamPage(
-        key: ValueKey('settings'),
-        child: SettingsPage(),
-      ),
-    ];
-  }
-}
+class _ScaffoldWithBottomNavBarState extends State<ScaffoldWithBottomNavBar> {
+  var routerDelegate = [
+    BeamerDelegate(
+        initialPath: '/a',
+        locationBuilder: (routerInformation, _) {
+          if (routerInformation.location!.contains('/a')) {
+            return (ALocation(routerInformation));
+          } else {
+            return NotFound(path: routerInformation.location!);
+          }
+        }),
+  ];
 
-class HomePage extends ConsumerWidget {
-  @override
-  Widget build(BuildContext context, ScopedReader watch) {
-    final selectedTabIndex = watch(selectedTabIndexProvider).state;
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: Column(
-        children: [
-          TabBar(
-            tabs: [
-              Tab(text: 'Tab 1'),
-              Tab(text: 'Tab 2'),
-            ],
-            onTap: (index) {
-              context.read(selectedTabIndexProvider).state = index;
-            },
-          ),
-          Expanded(
-            child: IndexedStack(
-              index: selectedTabIndex,
-              children: [
-                Tab1Page(),
-                Tab2Page(),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class Tab1Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Tab 1 Page'),
-    );
-  }
-}
-
-class Tab2Page extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('Tab 2 Page'),
-    );
-  }
-}
-
-class DetailsPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Details'),
-      ),
-      body: Center(
-        child: Text('Details Page'),
-      ),
-    );
-  }
-}
-
-class SettingsPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Settings'),
-      ),
-      body: Center(
-        child: Text('Settings Page'),
-      ),
-    );
+    return const Placeholder();
   }
 }
