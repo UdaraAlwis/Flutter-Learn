e6979b3508e54ecb8fa7801d52c21878c397ce46
Udara Alwis <udara.robotics@gmail.com>
2023-05-30 18:27:49 +0200

    flutter sliver sample

---
+++ flutter_slivers_demo_app/android/app/src/main/kotlin/com/example/flutter_slivers_demo_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_slivers_demo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_slivers_demo_app/lib/main.dart
@@ -0,0 +1,118 @@
+// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+void main() {
+  runApp(ShrinkWrApp());
+}
+
+class ShrinkWrApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ShrinkWrap vs Slivers',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("ShrinkWrap, Street Rat, I don't, Buy that!"),
+        ),
+        body: const ShrinkWrapSlivers(),
+      ),
+    );
+  }
+}
+
+class ShrinkWrapSlivers extends StatefulWidget {
+  const ShrinkWrapSlivers({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _ShrinkWrapSliversState createState() => _ShrinkWrapSliversState();
+}
+
+class _ShrinkWrapSliversState extends State<ShrinkWrapSlivers> {
+  List<SliverList> innerLists = [];
+  final numLists = 15;
+  final numberOfItemsPerList = 100;
+
+  @override
+  void initState() {
+    super.initState();
+    Random rand = Random(DateTime.now().millisecondsSinceEpoch);
+
+    for (int i = 0; i < numLists; i++) {
+      final _innerList = <ColorRow>[];
+      for (int j = 0; j < numberOfItemsPerList; j++) {
+        _innerList.add(const ColorRow());
+      }
+      innerLists.add(SliverList( 
+          delegate: SliverChildBuilderDelegate((context, index) => _innerList[index], childCount: _innerList.length)));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(slivers: innerLists);
+  }
+}
+
+@immutable
+class ColorRow extends StatefulWidget {
+  const ColorRow({Key? key}) : super(key: key);
+
+  @override
+  State createState() => ColorRowState();
+}
+
+class ColorRowState extends State<ColorRow> {
+  Color? color;
+
+  @override
+  void initState() {
+    super.initState();
+    color = randomColor();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('Building ColorRowState');
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            randomColor(),
+            randomColor(),
+          ],
+        ),
+      ),
+      child: Row(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(height: 50, width: 50, color: Colors.white),
+          ),
+          Flexible(
+            child: Column(
+              children: const <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Text('I\'m a widget!', style: TextStyle(color: Colors.white)),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Color randomColor() => Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
